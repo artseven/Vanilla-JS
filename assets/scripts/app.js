31c69f97b0b3acf19d559cd91ad12a5c4a47a581
artseven <yabkevich.artsiom.09@gmail.com>
2019-12-18 16:21:13 -0500

    upd

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add() {
-  currentResult = currentResult + parseInt(userInput.value);
-  outputResult(currentResult, '');
+function getUserNumberInput() {
+	return parseInt(userInput.value)
 }
 
-// currentResult = add(1, 5);
+function add() {
+	const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + enteredNumber;
+  outputResult(currentResult, calcDescription);
+}
 
 addBtn.addEventListener("click", add);
-
-// let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`;
