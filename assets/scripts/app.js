b166f2b3770811d5a4dda9e2fefede1fc7ef4f0a
artseven <yabkevich.artsiom.09@gmail.com>
2020-01-13 13:06:15 -0500

    finished

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -30,40 +30,43 @@ function writeToLog(
 }
 
 function calculateResult(calculationType) {
+  const enteredNumber = getUserNumberInput();
   if (
     calculationType !== "ADD" &&
     calculationType !== "SUBTRACT" &&
     calculationType !== "MULTIPLY" &&
-    calculationType !== "DIVIDE"
+    calculationType !== "DIVIDE" ||
+    !enteredNumber 
+    //enteredNumber is zero
   ) {
     return;
   }
-  if (
-    calculationType === "ADD" ||
-    calculationType === "SUBTRACT" ||
-    calculationType === "MULTIPLY" ||
-    calculationType === "DIVIDE"
-  ) {
-    const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult;
-    let mathOperator;
-    if (calculationType === "ADD") {
-      currentResult += enteredNumber;
-      mathOperator = "+";
-    } else if (calculationType === "SUBTRACT") {
-      currentResult -= enteredNumber;
-      mathOperator = "-";
-    } else if (calculationType === "MULTIPLY") {
-      currentResult *= enteredNumber;
-      mathOperator = "*";
-    } else if (calculationType === "DIVIDE") {
-      currentResult /= enteredNumber;
-      mathOperator = "/";
-    }
 
-    createAndWriteOutput(mathOperator, initialResult, enteredNumber);
-    writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+  // if (
+  //   calculationType === "ADD" ||
+  //   calculationType === "SUBTRACT" ||
+  //   calculationType === "MULTIPLY" ||
+  //   calculationType === "DIVIDE"
+  // ) {
+  const initialResult = currentResult;
+  let mathOperator;
+  if (calculationType === "ADD") {
+    currentResult += enteredNumber;
+    mathOperator = "+";
+  } else if (calculationType === "SUBTRACT") {
+    currentResult -= enteredNumber;
+    mathOperator = "-";
+  } else if (calculationType === "MULTIPLY") {
+    currentResult *= enteredNumber;
+    mathOperator = "*";
+  } else if (calculationType === "DIVIDE") {
+    currentResult /= enteredNumber;
+    mathOperator = "/";
   }
+
+  createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+  writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+  // }
 }
 
 function add() {
